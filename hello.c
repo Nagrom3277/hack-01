112fb99109d995fc3869344537ab1b7396312a29
Nagrom3277 <42652333+Nagrom3277@users.noreply.github.com>
2018-08-23 13:20:41 -0500

    code for hack 1

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Mason
+
+ * Date: 2018/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Mason Morgan Computer Engineering\n");
+
+  return 0;
+}
